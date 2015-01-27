ca3deb567e243dcdac6e333b5745153bd1015db3
bobbyhq <gabrielcmoraru@gmail.com>
2015-01-27 13:40:20 +0000

    ONLINE

---
+++ common/config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=yii2_start',
+    'username' => 'root',
+    'password' => 'asdfgh',
+    'charset' => 'utf8',
+    'tablePrefix' => 'yii2_start_'
+];
